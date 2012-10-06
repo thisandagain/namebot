dd5784268ebb73f3b391eecb574cc698288091db
Andrew Sliwinski <thisandagain@gmail.com>
2012-10-05 18:15:00 -0700

    Prepare for release

--- README.md
+++ README.md
@@ -1,58 +1,46 @@
-# Namebot
-#### A robot that spits out name combinations for you. Beep <buzz> ... *ding*!
+## Namebot
+#### A robot that streams name combinations for you. Beep <buzz> ... *ding*!
 
-## Install
-	npm install namebot
+### Install
+```bash
+npm install namebot
+```
 
-## Basic Use
+### Usage
 ```javascript
-var namebot = require('namebot');
+var Namebot = require('namebot');
 
-namebot({
-	base: 			['badger', 'panda']
-}, function (err, a) {
-	console.log(a);  			// Win!
+var stream = new Namebot({
+    base: ['cuttlefish', 'shrew']
+    count: 100
 });
+
+stream.pipe(process.stdout);
+stream.start();
 ````
 
-## Less Basic Use
-```javascript
-var namebot = require('namebot');
+### Binary Use
+```bash
+[sudo] npm install -g namebot
+namebot --base "['mega', 'shark', 'giant', 'octopus']" --count 25
+```
 
-namebot({
-	base: 			'http://domain.com/path/to/base.json',
-	dictionary: 	'./path/to/dictionary.dict',
-	method: 		['rhyme'],
-	count: 			10
-}, function (err, a) {
-	console.dir(a);  			// Win!
-});
-````
-
-## Binary Use
-	npm install -g namebot
-
-	namebot --base "['mega', 'shark', 'giant', 'octopus']" --count 25
-
-## Options
+### Options
 #### Base
 An array of words that form the basis for names. All names generated will include a word from this object. Can be specified as a local path or as a HTTP GET request to a JSON resource by passing along a URL string.
 
 #### Dictionary (Optional)
-A phonetic dictionary (see [CMUdict](http://www.speech.cs.cmu.edu/cgi-bin/cmudict) for reference). Can be specified as a local path or as a HTTP GET request to a resource by passing along a URL string.
-
-#### Method (Optional)
-An array of naming methods that will be used by the generator. Will default to use all available options if not set. Currently only supports two options:
-- Rhyme
-- Random
-
-*Note: Regardless of the method options, namebot will use the random method to generate a name if a rhyming pair can not be found.
+Local path to a phonetic dictionary (see [CMUdict](http://www.speech.cs.cmu.edu/cgi-bin/cmudict) for reference).
 
 #### Count (Optional)
 Number of names that will be generated. Will default to one (1) if not set.
 
-## Credits
-The rhyming method for namebot was derived from [James Halliday's](http://substack.net/) [node-rhyme](https://github.com/substack/node-rhyme) module.
+---
 
 ## Testing
-	npm test
+```bash
+npm test
+```
+
+## Credits
+The rhyming method for namebot was derived from [James Halliday's](http://substack.net/) [node-rhyme](https://github.com/substack/node-rhyme) module.
--- test/index.js
+++ test/index.js
@@ -1,167 +1,64 @@
-var vows    = require('vows'),
-    assert  = require('assert'),
-    namebot = require('../lib/index.js');
+/**
+ * Test suite
+ *
+ * @package namebot
+ * @author Andrew Sliwinski <andrew@diy.org>
+ */
+
+/**
+ * Dependencies
+ */
+var async   = require('async'),
+    test    = require('tap').test,
+
+    target  = require(__dirname + '/../lib/index.js');
 
 /**
  * Setup
  */
-var suite       = vows.describe('NameBot');
-var base        = ['FOX', 'TURTLE', 'OWL', 'HOOT', 'WISE', 'BISON'];
-var dictionary  = './dict/cmudict.07a.dict';
+var count   = 10;
+var namebot = new target({
+    base: ['FOX', 'TURTLE', 'OWL', 'HOOT', 'WISE', 'BISON'],
+    count: count
+});
 
 /**
- * Test
+ * Suite
  */
-suite.addBatch({
+async.auto({
 
-    'Generic': {
-
-        topic: function() {
-            namebot({
-                base:           base,
-            }, this.callback);
-        },
-
-        'is a string': function (err, obj) {
-            assert.isString(obj);
-        },
-
-        'includes two words': function (err, obj) {
-            assert.strictEqual(2, obj.split(' ').length);
-        },
-
-        'includes a base from corpus': function (err, obj) {
-            assert.include(base, obj.split(' ')[0]);
-        }
-
-    },
-
-    'Using only the rhyme method': {
-
-        topic: function() {
-            namebot({
-                base:           base,
-                dictionary:     dictionary,
-                method:         ['rhyme'],
-            }, this.callback);
-        },
-
-        'is a string': function (err, obj) {
-            assert.isString(obj);
-        },
+    general:  function (callback) {
+        var a = [];
         
-        'includes two words': function (err, obj) {
-            assert.strictEqual(2, obj.split(' ').length);
-        },
+        namebot.on('data', function (data) {
+            a.push(data);
+        });
 
-        'includes a base from corpus': function (err, obj) {
-            assert.include(base, obj.split(' ')[0]);
-        }
+        namebot.on('end', function () {
+            callback(null, a);
+        });
 
+        namebot.start();
     },
 
-    'Using only the rhyme method (x10)': {
+    test:   ['general', function (callback, obj) {
+        test('Component definition', function (t) {
+            t.type(target, 'function', 'Component should be a function');
+            t.end();
+        });
 
-        topic: function() {
-            namebot({
-                base:           base,
-                dictionary:     dictionary,
-                method:         ['rhyme'],
-                count:          10
-            }, this.callback);
-        },
+        test('General test case', function (t) {
+            t.type(obj.general, 'object', 'Results should be an object');
+            t.equal(obj.general.length, count, 'Reults should be of expected length');
+            t.end();
+        });
 
-        'is an array': function (err, obj) {
-            assert.isArray(obj);
-        },
+        callback();
+    }]
 
-        'is the proper length': function (err, obj) {
-            assert.strictEqual(obj.length, 10);
-        },
-
-        'first item is a string': function (err, obj) {
-            assert.isString(obj[0]);  
-        },
-        
-        'first item includes two words': function (err, obj) {
-            assert.strictEqual(2, obj[0].split(' ').length);
-        },
-
-        'first item includes a base from corpus': function (err, obj) {
-            assert.include(base, obj[0].split(' ')[0]);
-        }
-
-    },
-
-    'Using only the random method (x10)': {
-
-        topic: function() {
-            namebot({
-                base:           base,
-                dictionary:     dictionary,
-                method:         ['random'],
-                count:          10
-            }, this.callback);
-        },
-
-        'is an array': function (err, obj) {
-            assert.isArray(obj);
-        },
-
-        'is the proper length': function (err, obj) {
-            assert.strictEqual(obj.length, 10);
-        },
-
-        'first item is a string': function (err, obj) {
-            assert.isString(obj[0]);  
-        },
-        
-        'first item includes two words': function (err, obj) {
-            assert.strictEqual(2, obj[0].split(' ').length);
-        },
-
-        'first item includes a base from corpus': function (err, obj) {
-            assert.include(base, obj[0].split(' ')[0]);
-        }
-
-    },
-
-    'Using only the random method (x10)': {
-
-        topic: function() {
-            namebot({
-                base:           base,
-                dictionary:     dictionary,
-                method:         ['random'],
-                count:          10
-            }, this.callback);
-        },
-
-        'is an array': function (err, obj) {
-            assert.isArray(obj);
-        },
-
-        'is the proper length': function (err, obj) {
-            assert.strictEqual(obj.length, 10);
-        },
-
-        'first item is a string': function (err, obj) {
-            assert.isString(obj[0]);  
-        },
-        
-        'first item includes two words': function (err, obj) {
-            assert.strictEqual(2, obj[0].split(' ').length);
-        },
-
-        'first item includes a base from corpus': function (err, obj) {
-            assert.include(base, obj[0].split(' ')[0]);
-        }
-
-    },
-
-})
-
-/**
- * Export
- */
-.export(module);
+}, function (err, obj) {
+    test('Catch errors', function (t) {
+        t.equal(err, null, 'Errors should be null');
+        t.end();
+    });
+});
